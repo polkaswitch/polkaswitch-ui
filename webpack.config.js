d4f0e1f2bcf0eca6e1c2092117cc437e856467d5
Yuri Ramos <yuri.pramos@gmail.com>
2022-07-22 11:10:36 +0200

    Modify `webpack.config.js`

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
-const SentryWebpackPlugin = require("@sentry/webpack-plugin");
-require('dotenv').config()
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
+require('dotenv').config();
 
 const outputDirectory = 'dist';
 
@@ -28,11 +28,11 @@ module.exports = (env) => {
   let plugins = [
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
+      filename: '[name].[contenthash].css',
     }),
     new HtmlWebpackPlugin({
       template: './src/client/index.html',
-      hash: false
+      hash: false,
     }),
     new webpack.EnvironmentPlugin({
       IS_PRODUCTION: !!isProduction,
@@ -40,28 +40,26 @@ module.exports = (env) => {
       IS_CLAIM_DOMAIN: isClaimDomain,
       SENTRY_JS_DSN: false,
       HEROKU_RELEASE_VERSION: false,
-      HEROKU_APP_NAME: false
+      HEROKU_APP_NAME: false,
     }),
-    new NodePolyfillPlugin()
+    new NodePolyfillPlugin(),
   ];
 
-  if (isProduction &&
-    process.env.HEROKU_APP_NAME &&
-    process.env.SENTRY_AUTH_TOKEN) {
+  if (isProduction && process.env.HEROKU_APP_NAME && process.env.SENTRY_AUTH_TOKEN) {
     plugins.push(
       new SentryWebpackPlugin({
         authToken: process.env.SENTRY_AUTH_TOKEN,
-        org: "swing-xyz",
-        project: "frontend",
-        release: process.env.HEROKU_APP_NAME + "-" + process.env.HEROKU_RELEASE_VERSION,
+        org: 'swing-xyz',
+        project: 'frontend',
+        release: process.env.HEROKU_APP_NAME + '-' + process.env.HEROKU_RELEASE_VERSION,
         deploy: {
-          env: isMainNetwork ? 'production' : 'development'
+          env: isMainNetwork ? 'production' : 'development',
         },
         dryRun: !isProduction,
 
         // webpack-specific configuration
-        include: "./dist"
-      })
+        include: './dist',
+      }),
     );
   } else {
     console.log('Sentry not configured - skipped');
@@ -71,7 +69,7 @@ module.exports = (env) => {
     entry: ['babel-polyfill', './src/client/js/index.js'],
     output: {
       path: path.join(__dirname, outputDirectory),
-      filename: 'bundle.[contenthash].js'
+      filename: 'bundle.[contenthash].js',
     },
     module: {
       rules: [
@@ -79,53 +77,48 @@ module.exports = (env) => {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader'
-          }
+            loader: 'babel-loader',
+          },
         },
         {
           test: /\.(scss|css)$/,
           use: [
             MiniCssExtractPlugin.loader,
-            {
-              loader: 'css-loader'
-            },
+            'css-loader',
             {
               loader: 'sass-loader',
               options: {
                 sourceMap: true,
-              }
-            }
-          ]
+              },
+            },
+          ],
         },
         {
-          test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/,
-          use: [
-            'file-loader'
-          ]
-        }
-      ]
+          test: /\.(png|jpg|gif|woff|woff2|eot|ttf|otf|svg)$/,
+          use: ['file-loader'],
+        },
+      ],
     },
     resolve: {
       extensions: ['*', '.js', '.jsx'],
       fallback: {
         fs: false,
         net: false,
-        tls: false
-      }
+        tls: false,
+      },
     },
     devServer: {
       port: 3000,
       open: true,
       historyApiFallback: true,
       proxy: {
-        '/api': 'http://localhost:8080'
-      }
+        '/api': 'http://localhost:8080',
+      },
     },
-    devtool : isProduction ? 'source-map' : 'inline-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     plugins: plugins,
     experiments: {
-      topLevelAwait: true
-    }
+      topLevelAwait: true,
+    },
   };
 };
-
